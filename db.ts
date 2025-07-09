7aaaed218e8cc5b1de190cd764c841d3023998f5
Eduardo Matuella <eduardomatuella@gmail.com>
2025-07-09 14:08:14 -0300

    s

--- db.ts
+++ db.ts
@@ -1,10 +1,18 @@
-// Exemplo de conexão com SQLite usando sqlite3 em TypeScript
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_CONNECTION
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+console.log('INFO CONNECTION:', pool);
 
 export default pool;
